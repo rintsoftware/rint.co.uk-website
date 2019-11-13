59843152027c30b482269fe4263b2da0c1c99b44
Ilker Mutlu <ilker.mutlu@gmail.com>
2019-11-13 16:24:34 +0000

    Apply master layout to existing templates.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,120 +1,110 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
+@section('styles')
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #25313D;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-        <title>Rint Software Limited</title>
+        .full-height {
+            height: 100vh;
+        }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #25313D;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .links > a {
+            color: #ebc445;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        img.logo {
+            margin: auto;
+            width: 250px;
+            height: 250px;
+        }
 
-            .links > a {
-                color: #ebc445;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        h2 {
+            color: #ebc445;
+            font-size: 0.3em;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+        .servicesContainer {
+            display: flex;
+            width: 90vw;
+            justify-content: space-between;
+            flex-wrap: wrap;
+        }
 
-            img.logo {
-                margin: auto;
-                width: 250px;
-                height: 250px;
-            }
+        .servicesContainer > div {
+            width: 100%;
+        }
 
-            h2 {
-                color: #ebc445;
-                font-size: 0.3em;
-            }
+        .servicesContainer p {
+            color: white;
+            font-size: 0.25em;
+        }
+    </style>
+@endsection
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="top-right links">
+            <a href="{{ route('home') }}">Homepage</a>
+            <a href="{{ route('services') }}">Services</a>
+            <a href="{{ route('about') }}">About</a>
+            <a href="{{ route('contact') }}">Contact</a>
+        </div>
 
-            .servicesContainer {
-                display: flex;
-                width: 90vw;
-                justify-content: space-between;
-                flex-wrap: wrap;
-            }
-
-            .servicesContainer > div {
-                width: 100%;
-            }
-
-            .servicesContainer p {
-                color: white;
-                font-size: 0.25em;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="top-right links">
-                <a href="{{ route('home') }}">Homepage</a>
-                <a href="{{ route('services') }}">Services</a>
-                <a href="{{ route('about') }}">About</a>
-                <a href="{{ route('contact') }}">Contact</a>
-            </div>
-
-            <div class="content">
-                <div class="title m-b-md">
-                    <img class="logo" src="images/logo.png" alt="Rint Software Limited">
-                    <div class="servicesContainer">
-                        <div>
-                            <h2>Contact</h2>
-                            <p>
-                                Interested in our services?
-                            </p>
-                            <p>
-                                Drop us an email at <strong>ilker[at]rint.co.uk</strong>
-                            </p>
-                        </div>
+        <div class="content">
+            <div class="title m-b-md">
+                <img class="logo" src="images/logo.png" alt="Rint Software Limited">
+                <div class="servicesContainer">
+                    <div>
+                        <h2>Contact</h2>
+                        <p>
+                            Interested in our services?
+                        </p>
+                        <p>
+                            Drop us an email at <strong>ilker[at]rint.co.uk</strong>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,91 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
+@section('styles')
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #25313D;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-        <title>Rint Software Limited</title>
+        .full-height {
+            height: 100vh;
+        }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #25313D;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .links > a {
+            color: #ebc445;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        img.logo {
+            margin: auto;
+            width: 400px;
+            height: 400px;
+        }
+    </style>
+@endsection
+@section('content')
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-            .links > a {
-                color: #ebc445;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+        <div class="content">
+            <div class="title m-b-md">
+                <img class="logo" src="images/logo.png" alt="Rint Software Limited">
+            </div>
 
-            img.logo {
-                margin: auto;
-                width: 400px;
-                height: 400px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    <img class="logo" src="images/logo.png" alt="Rint Software Limited">
-                </div>
-
-                <div class="links">
-                    <a href="{{ route('services') }}">Services</a>
-                    <a href="{{ route('about') }}">About</a>
-                    <a href="{{ route('contact') }}">Contact</a>
-                </div>
+            <div class="links">
+                <a href="{{ route('services') }}">Services</a>
+                <a href="{{ route('about') }}">About</a>
+                <a href="{{ route('contact') }}">Contact</a>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
